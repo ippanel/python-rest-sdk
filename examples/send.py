14d420b9802797ea705572833c5b5147acf8c796
Mohsen Saberi <mohsen.saberi@gmail.com>
2022-09-19 14:26:33 +0430

    fixing bug

--- examples/send.py
+++ examples/send.py
@@ -1,9 +1,9 @@
 from ippanel import Client, Error, HTTPError, ResponseCode
 
-client = Client("YOUR-API-KEY")
+client = Client("bETP93HSkawWB4FdCMfoJ7SECKUj2BgiSw0qeZi2CBI=")
 
 try:
-    message_id = client.send("+9810001", ["+98912xxxxxxx"], "Hello from python client!")
+    message_id = client.send("+983000505", ["+989153621841"], "Hello from python client!", "summary")
     print(message_id)
 except Error as e:
     print("Error handled => code: %s, message: %s" % (e.code, e.message))
